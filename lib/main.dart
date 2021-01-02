1bef999fbf42665983ed24116495566411cd3a0f
probe2k <kyash054@gmail.com>
2021-01-02 20:02:52 +0530

    ThemeData add | todo: toggle switch

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.grey,
+        scaffoldBackgroundColor: Colors.black,
+        buttonTheme: ButtonThemeData(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0),),
+          buttonColor: Colors.purpleAccent,
+        ),
+      ),
       home: AppHome(),
       debugShowCheckedModeBanner: false,
     );
