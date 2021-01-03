8a711fc0939682006f2beb8dee4ba9e08247c47b
probe2k <kyash054@gmail.com>
2021-01-03 17:30:39 +0530

    widget state revamp | interaction progression

--- lib/uidai_webview.dart
+++ lib/uidai_webview.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:async';
 
-class AadharWebview extends StatelessWidget {
+class AadharWebview extends StatefulWidget {
+  @override
+  _AadharWebviewState createState() => _AadharWebviewState();
+}
+
+class _AadharWebviewState extends State<AadharWebview> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      title: Text(
-        'Aadhar Verification',
-      ),
-      centerTitle: true,
-    ));
+    return Scaffold();
   }
 }
