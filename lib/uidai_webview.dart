a442e5b79ebcdaf97a52de08d7bfca1c05423771
probe2k <kyash054@gmail.com>
2021-01-03 17:48:32 +0530

    WebView design and default URL tag

--- lib/uidai_webview.dart
+++ lib/uidai_webview.dart
@@ -2,14 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:async';
 
-class AadharWebview extends StatefulWidget {
-  @override
-  _AadharWebviewState createState() => _AadharWebviewState();
-}
+class AadharWebView extends StatelessWidget {
+  String url;
 
-class _AadharWebviewState extends State<AadharWebview> {
+  AadharWebView({this.url});
+
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Aadhar Verification',
+        ),
+        centerTitle: true,
+      ),
+      body: Builder(
+        builder: (BuildContext context) {
+          return WebView(
+            initialUrl: 'https://resident.uidai.gov.in/verify',
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.complete(webViewController);
+            },
+            navigationDelegate: (NavigationRequest request) {
+              print('allowing navigation to $request');
+              return NavigationDecision.navigate;
+            },
+            onPageStarted: (String url) {
+              print('Page loading: $url');
+            },
+            onPageFinished: (String url) {
+              print('Page loaded: $url');
+            },
+            gestureNavigationEnabled: true,
+          );
+        },
+      ),
+    );
   }
 }
